c35600f5a1de0fed6f0b4a874d77ff750e70a669
Thanhhaitvu1707 <haitt170702@tvu-onschool.edu.vn>
2025-04-14 00:42:52 +0700

    Add files via upload

---
+++ Scripts/active-menu.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    
+    let currentPath = window.location.pathname.toLowerCase();
+    console.log("Current Path:", currentPath);
+
+    let navLinks = document.querySelectorAll("#navMenu .nav-link");
+
+    navLinks.forEach(link => {
+        let linkPath = new URL(link.href, window.location.origin).pathname.toLowerCase();
+    
+
+        // So sánh bằng cách loại bỏ ".aspx" nếu có
+        let normalizedCurrent = currentPath.replace(".aspx", "");
+        let normalizedLink = linkPath.replace(".aspx", "");
+
+        if (normalizedCurrent === normalizedLink) {
+            link.classList.add("active");
+      
+        }
+    });
+});
---
+++ Scripts/default.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    let images = document.querySelectorAll(".slider img");
+    let indicatorsContainer = document.querySelector(".slider-indicators");
+    let index = 0;
+
+    // Tạo vòng tròn chỉ số dựa trên số lượng ảnh
+    images.forEach((_, i) => {
+        let span = document.createElement("span");
+        if (i === 0) span.classList.add("active");
+        span.dataset.index = i;
+        indicatorsContainer.appendChild(span);
+    });
+
+    let indicators = document.querySelectorAll(".slider-indicators span");
+
+    function showImage(i) {
+        images[index].classList.remove("active");
+        indicators[index].classList.remove("active");
+
+        index = i;
+
+        images[index].classList.add("active");
+        indicators[index].classList.add("active");
+    }
+
+    function showNextImage() {
+        let nextIndex = (index + 1) % images.length;
+        showImage(nextIndex);
+    }
+
+    // Tự động chuyển ảnh sau 3 giây
+    setInterval(showNextImage, 3000);
+
+   
+   
+});
